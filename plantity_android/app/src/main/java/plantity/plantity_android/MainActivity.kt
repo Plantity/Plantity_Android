c7a70b61936fdfd8d6ed8210366fd7ea94b17590
Gamza <flsbdpt194@naver.com>
2022-07-22 21:11:25 +0900

    Init Project
    안드로이드 프로젝트 생성

---
+++ plantity_android/app/src/main/java/plantity/plantity_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package plantity.plantity_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

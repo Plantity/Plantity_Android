705ef40c44eeeb97f547915e96837fc6621eaf21
skfl0282 <skfl0282@gmail.com>
2022-08-05 17:41:32 +0900

    Add_design activity_mypage.xml

---
+++ plantity_android/app/src/main/java/plantity/plantity_android/MyplantsFragment.kt
@@ -0,0 +1,20 @@
+package plantity.plantity_android
+
+import android.R
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView.ScaleType
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+
+
+class MyplantsFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}

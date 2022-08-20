fff3db13ad6159e84a6b97bb76c6b242ab8e5dcb
Gamza <flsbdpt194@naver.com>
2022-08-20 20:22:33 +0900

    [DEL] 기존에 생성해뒀던 어댑터 및 액티비티 일부 삭제 - 혼동 때문에 [ADD] guideline Sample값이 담긴 res/values/guideLineSample.xml 추가

--- plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineActivity.kt
+++ plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineActivity.kt
@@ -3,10 +3,18 @@ package plantity.plantity_android.guideline
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import plantity.plantity_android.R
+import plantity.plantity_android.databinding.ActivityGuideLineBinding
 
 class GuideLineActivity : AppCompatActivity() {
+    //뷰 바인딩
+    private var mBinding: ActivityGuideLineBinding? = null
+    private val binding get() = mBinding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guide_line)
+        mBinding = ActivityGuideLineBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
     }
 }

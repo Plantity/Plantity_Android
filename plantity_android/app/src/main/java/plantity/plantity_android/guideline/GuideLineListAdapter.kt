1b524c137894f40068ed2b732b83cf9f85a4a70a
Gamza <flsbdpt194@naver.com>
2022-09-13 20:59:00 +0900

    [fix]가이드라인 어댑터 수정 - 가이드라인 어댑터까지는 수정하였음! 컴파일러 업데이트가 필요하다고 함

--- plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineListAdapter.kt
+++ plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineListAdapter.kt
@@ -1,14 +1,33 @@
 package plantity.plantity_android.guideline
 
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import plantity.plantity_android.databinding.GuidelineCardCellBinding
 
-class GuideLineListAdapter(val titleList : Array<String>, val desList : Array<String>) :
-    RecyclerView.Adapter<GuideLineListAdapter.MyViewHolder>() {
+class GuideLineListAdapter(val gdlist: List<GuideData>) : RecyclerView.Adapter<GuideLineListAdapter.GuideViewHolder>(){
+    inner class GuideViewHolder(val binding: GuidelineCardCellBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : GuideViewHolder
+       = GuideViewHolder(GuidelineCardCellBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+
+
+    override fun onBindViewHolder(holder: GuideViewHolder, position: Int) {
+        Log.d("리사이클러뷰 call","onBindView Holder:$position")
+        val binding = (holder as GuideViewHolder).binding
+        //뷰에 데이터 출력
+        binding.gdCardTitle.text = gdlist[position].toString()
+        binding.cardview.setOnClickListener{
+            Log.d("cardClick", "item root click:$position")
+        }
+    }
+
+    override fun getItemCount(): Int = gdlist.size
+
+}
+  /* RecyclerView.Adapter<GuideLineListAdapter.MyViewHolder>() {
 
         class MyViewHolder(val binding: GuidelineCardCellBinding) :RecyclerView.ViewHolder(binding.root){
             fun bind(title: String, des: String) {
@@ -40,4 +59,4 @@ class GuideLineListAdapter(val titleList : Array<String>, val desList : Array<St
     }
 
 
-}
+}*/

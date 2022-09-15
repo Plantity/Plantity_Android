952aa26f5f9234ba533ea8fe4b824b9b0b2b418f
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2022-09-15 20:08:19 +0900

    fix 메인페이지

--- plantity_android/app/src/main/java/plantity/plantity_android/mypages/MyplantsFragment.kt
+++ plantity_android/app/src/main/java/plantity/plantity_android/mypages/MyplantsFragment.kt
@@ -1,11 +1,17 @@
 package plantity.plantity_android.mypages
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_like.*
+import plantity.plantity_android.PlantDetailActivity
 import plantity.plantity_android.R
+import plantity.plantity_android.plantlogs.PlantLogActivity
 
 
 class MyplantsFragment : Fragment() {
@@ -19,7 +25,24 @@ class MyplantsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_myplants, container, false)
+        val view = inflater.inflate(R.layout.fragment_myplants, container, false)
+        val fir : ImageView = view.findViewById(R.id.fir)
+        fir.setBackgroundResource(R.drawable.ic_plant_log_image)
+        fir.setOnClickListener{
+            val intent = Intent(activity, PlantLogActivity::class.java)
+            startActivity(intent)
+        }
+
+        val imageUrl2 = "https://cdn.pixabay.com/photo/2017/07/27/11/14/flower-2544986_960_720.jpg"
+        val sec : ImageView = view.findViewById(R.id.sec)
+        Glide.with(this).load(imageUrl2).into(sec)
+        sec.setOnClickListener{
+            val intent = Intent(activity, PlantLogActivity::class.java)
+            startActivity(intent)
+        }
+        val thi : ImageView = view.findViewById(R.id.thi)
+        thi.setEnabled(false)
+        return view
 
     }
 

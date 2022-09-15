7de2708fb03c563d68c31b3ecb58d8b4fb0bf124
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2022-09-15 21:12:13 +0900

    fix 메인페이지

--- plantity_android/app/src/main/java/plantity/plantity_android/mypages/MyplantsFragment.kt
+++ plantity_android/app/src/main/java/plantity/plantity_android/mypages/MyplantsFragment.kt
@@ -27,13 +27,14 @@ class MyplantsFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_myplants, container, false)
         val fir : ImageView = view.findViewById(R.id.fir)
-        fir.setBackgroundResource(R.drawable.ic_plant_log_image)
+        val imageUrl1 = "https://www.100ssd.co.kr/news/photo/202009/71614_51734_4048.jpg"
+        Glide.with(this).load(imageUrl1).into(fir)
         fir.setOnClickListener{
             val intent = Intent(activity, PlantLogActivity::class.java)
             startActivity(intent)
         }
 
-        val imageUrl2 = "https://cdn.pixabay.com/photo/2017/07/27/11/14/flower-2544986_960_720.jpg"
+        val imageUrl2 =  "https://mule4.dingul.io/api/r?l=aHR0cHM6Ly90aHVtYm5haWw5LmNvdXBhbmdjZG4uY29tL3RodW1ibmFpbHMvcmVtb3RlLzQ5Mng0OTJleC9pbWFnZS92ZW5kb3JfaW52ZW50b3J5L2U1ZWMvNGI5YzQxODdjMjYyZGZiOGY2NzIyMmQzZDIzNWVhODU2YjA1NTViYWI2N2IwMTE4MDk5ZDlmMjI5OGFjLmpwZw"
         val sec : ImageView = view.findViewById(R.id.sec)
         Glide.with(this).load(imageUrl2).into(sec)
         sec.setOnClickListener{

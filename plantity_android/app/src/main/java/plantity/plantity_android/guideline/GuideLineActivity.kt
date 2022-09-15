5006fb92d4f56dde8cfee69c7c931378d1fc80c2
Suyeon <syhhwang1231@gmail.com>
2022-09-15 20:53:53 +0900

    Fix: 가이드라인 리사이클러뷰 수정
    - 임시 수정입니다

--- plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineActivity.kt
+++ plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineActivity.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import plantity.plantity_android.NavBarFragment
 import plantity.plantity_android.R
 import plantity.plantity_android.databinding.ActivityGuideLineBinding
+import plantity.plantity_android.search.SearchAdapter
 
 class GuideLineActivity : AppCompatActivity() {
     //뷰 바인딩
@@ -18,13 +19,20 @@ class GuideLineActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = ActivityGuideLineBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val listdata = mutableListOf<GuideData>()
+        //val listdata = mutableListOf<GuideData>() -> 원래 코드에 있던 것
+        val guideLineDataList = mutableListOf<GuideLineData>()
+        guideLineDataList.add(GuideLineData("가이드라인 제목", "내용입니달라........................"))
+        guideLineDataList.add(GuideLineData("가이드라인 제목2", "내용2입니달라......................"))
+        guideLineDataList.add(GuideLineData("가이드라인 제목3", "내용3입니달라..........................."))
 
         binding.gdRecyclerView.layoutManager = LinearLayoutManager(this)
-        binding.gdRecyclerView.adapter=GuideLineListAdapter(listdata)
+        //binding.gdRecyclerView.adapter=GuideLineListAdapter(listdata)
         binding.gdRecyclerView.addItemDecoration(DividerItemDecoration(this,LinearLayoutManager.VERTICAL))
         setNavBarFragment("guide")
-        
+
+        val guideLineAdatper = GuideLineAdapter(guideLineDataList)  // 어댑터
+        binding.gdRecyclerView.adapter = guideLineAdatper
+//        binding.searchListRecyclerView.layoutManager = LinearLayoutManager(guide)  // context로 this가 아님?
     }
 
     fun setNavBarFragment(title:String){

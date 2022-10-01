8e1f823e19017a1afb39c5a8fb1f6b63c01393dc
Gamza <flsbdpt194@naver.com>
2022-10-01 21:41:15 +0900

    [add] 닫기 버튼에 기능 추가 - 열려있는 식물 추가 프래그먼트를 닫음

--- plantity_android/app/src/main/java/plantity/plantity_android/main/AddPlantFragment.kt
+++ plantity_android/app/src/main/java/plantity/plantity_android/main/AddPlantFragment.kt
@@ -1,22 +1,25 @@
 package plantity.plantity_android.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import plantity.plantity_android.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import plantity.plantity_android.databinding.FragmentAddPlantBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddPlantFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+//뷰바인딩
+private var mBinding : FragmentAddPlantBinding?=null
+private val binding get() = mBinding!!
+
 class AddPlantFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -24,30 +27,31 @@ class AddPlantFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        
+        //X 클릭시 닫기
+        binding.addViewClosebutton.setOnClickListener(View.OnClickListener {
+            val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+            fragmentManager.beginTransaction().remove(this).commit()
+            fragmentManager.popBackStack()
+        })
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_plant, container, false)
+        mBinding = FragmentAddPlantBinding.inflate(layoutInflater)
+        Log.d("binding","바인딩 완료")
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        mBinding = null
+        super.onDestroyView()
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddPlantFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             AddPlantFragment().apply {
--- plantity_android/app/src/main/java/plantity/plantity_android/main/MainActivity.kt
+++ plantity_android/app/src/main/java/plantity/plantity_android/main/MainActivity.kt
@@ -10,7 +10,7 @@ import android.widget.ImageView
 import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+//import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_main_card.*
 import plantity.plantity_android.MainLogFragment

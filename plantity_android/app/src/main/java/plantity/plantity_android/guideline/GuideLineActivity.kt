a729fa686ddf7dd6ff66bab6620840a36d1b9d3b
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2022-10-02 16:58:47 +0900

    Guideline

--- plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineActivity.kt
+++ plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineActivity.kt
@@ -1,23 +1,67 @@
 package plantity.plantity_android.guideline
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_guide_line.*
 import plantity.plantity_android.NavBarFragment
 import plantity.plantity_android.R
 import plantity.plantity_android.databinding.ActivityGuideLineBinding
 
+private const val NUM_PAGES = 5
 class GuideLineActivity : AppCompatActivity() {
     //뷰 바인딩
+    lateinit var guidelinelistAdapter : GuideLineListAdapter
+
     private var mBinding: ActivityGuideLineBinding? = null
     private val binding get() = mBinding!!
-
-
+    private lateinit var cardviewPager : ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityGuideLineBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setNavBarFragment("guide")
-        
+        cardviewPager = mBinding!!.pager
+        val cardViewAdapter = cardPagerAdapter(this)
+
+        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.pageMargin) // dimen 파일 안에 크기를 정의해두었다.
+        val pagerWidth = resources.getDimensionPixelOffset(R.dimen.pageWidth) // dimen 파일이 없으면 생성해야함
+        val screenWidth = resources.displayMetrics.widthPixels // 스마트폰의 너비 길이를 가져옴
+        val offsetPx = screenWidth - pageMarginPx - pagerWidth
+
+        cardviewPager.setPageTransformer { page, position ->
+            page.translationX = position * -offsetPx
+        }
+
+        cardviewPager.offscreenPageLimit = 2
+        cardviewPager.adapter = cardViewAdapter
+
+        initRecycler()
+        guideline_rv.addItemDecoration(VerticalItemDecorator(20))
+        guideline_rv.addItemDecoration(HorizontalItemDecorator(10))
+
+    }
+    private fun initRecycler() {
+
+        val list = ArrayList<GuidelineData>()
+        list.add(GuidelineData(title = "왕초보 식집사를 위한 기본 지식", subtitle = "식물 키우기가 처음이라면"))
+        list.add(GuidelineData(title = "실내식물 가지치기 하는 방법", subtitle = ""))
+        list.add(GuidelineData(title = "과습된 식물 살리는 방법", subtitle = "걱정이 지나쳐 물을 자주 주게 돼요."))
+        list.add(GuidelineData(title = "실내에서 선인장 기르는 방법", subtitle = ""))
+        list.add(GuidelineData(title = "식물 건강하게 키우는 방법", subtitle = ""))
+        list.add(GuidelineData(title = "실내식물 관리하는 방법", subtitle = ""))
+        list.add(GuidelineData(title = "다육이 잎꽂이 하는 방법", subtitle = ""))
+        list.add(GuidelineData(title = "분갈이 하는 방법", subtitle = ""))
+
+        guidelinelistAdapter = GuideLineListAdapter(list)
+        mBinding!!.guidelineRv.adapter = guidelinelistAdapter
+
     }
 
     fun setNavBarFragment(title:String){
@@ -29,4 +73,30 @@ class GuideLineActivity : AppCompatActivity() {
         transaction.add(R.id.nav_bar,navBarFragment)
         transaction.commit()
     }
+    private inner class cardPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = NUM_PAGES
+
+        override fun createFragment(position: Int): Fragment = GuidelineNewsFragment()
+    }
+
 }
+class HorizontalItemDecorator(private val divHeight : Int) : RecyclerView.ItemDecoration() { // 리사이클러뷰 간격 조절
+
+    @Override
+    override fun getItemOffsets(outRect: Rect, view: View, parent : RecyclerView, state : RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.left = divHeight
+        outRect.right = divHeight
+    }
+}
+class VerticalItemDecorator(private val divHeight : Int) : RecyclerView.ItemDecoration() {
+
+    @Override
+    override fun getItemOffsets(outRect: Rect, view: View, parent : RecyclerView, state : RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.top = divHeight
+        outRect.bottom = divHeight
+    }
+}
+
+
--- plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineListAdapter.kt
+++ plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineListAdapter.kt
@@ -1,43 +1,54 @@
 package plantity.plantity_android.guideline
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Rect
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.NonDisposableHandle.parent
+import plantity.plantity_android.PlantDetailActivity
+import plantity.plantity_android.R
 import plantity.plantity_android.databinding.GuidelineCardCellBinding
 
-class GuideLineListAdapter(val titleList : Array<String>, val desList : Array<String>) :
-    RecyclerView.Adapter<GuideLineListAdapter.MyViewHolder>() {
+class GuideLineListAdapter(private val data: ArrayList<GuidelineData>) : RecyclerView.Adapter<GuideLineListAdapter.ViewHolder>() {
 
-        class MyViewHolder(val binding: GuidelineCardCellBinding) :RecyclerView.ViewHolder(binding.root){
-            fun bind(title: String, des: String) {
-                binding.gdCardTitle.text=title
-                binding.gdCardContext.text=des
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.guideline_card_cell,parent,false)
+        return ViewHolder(view)
+    }
 
-                binding.cardview.setOnClickListener {
-                    val intent:Intent = Intent(it.context, GuideLineDetailActivity::class.java)
-                    intent.putExtra("currentTitle",title)
-                    intent.putExtra("currentDes",des)
-                    it.context.startActivity(intent)
-                }
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val tv_title: TextView = itemView.findViewById(R.id.tv_title)
+        private val tv_subtitle: TextView = itemView.findViewById(R.id.tv_subtitle)
+
+        fun bind(item: GuidelineData) {
+            tv_title.text = item.title
+            tv_subtitle.text = item.subtitle
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, GuideLineDetailActivity::class.java)
+                intent.putExtra("title", item.title)
+                ContextCompat.startActivity(itemView.context, intent, null)
+            }
         }
-
-
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val binding = GuidelineCardCellBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return MyViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.bind(titleList[position],desList[position])
-    }
-
-    override fun getItemCount(): Int {
-        return titleList.size
     }
 
 
 }
+data class GuidelineData(
+    val title : String,
+    val subtitle : String
+)
+

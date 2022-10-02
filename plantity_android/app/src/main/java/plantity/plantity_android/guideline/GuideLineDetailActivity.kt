7ecb1d7f7779b45b82c046b8a9b04d7e77acd8f6
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2022-10-02 17:30:13 +0900

    guide

--- plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineDetailActivity.kt
+++ plantity_android/app/src/main/java/plantity/plantity_android/guideline/GuideLineDetailActivity.kt
@@ -1,106 +1,54 @@
 package plantity.plantity_android.guideline
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_guide_line_detail.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.Rect
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.NonDisposableHandle.parent
+import plantity.plantity_android.PlantDetailActivity
 import plantity.plantity_android.R
+import plantity.plantity_android.databinding.GuidelineCardCellBinding
 
-class GuideLineDetailActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guide_line_detail)
+class GuideLineListAdapter(private val data: ArrayList<GuidelineData>) : RecyclerView.Adapter<GuideLineListAdapter.ViewHolder>() {
 
-        //intent
-        val titlename = intent.getStringExtra("title")
-
-        if(titlename == "실내식물 가지치기 하는 방법"){
-            bigtitle.setText(titlename)
-            subtitle1.setText("1. 죽은 잎과 가지, 꽃 제거")
-            content1.setText(R.string.guide1_1)
-            image1.setImageResource(R.drawable.kakao_login_page)
-            subtitle2.setText("2. 지나치게 많이 자란 가지와 줄기 잘라내기")
-            content2.setText(R.string.guide1_2)
-            image2.setImageResource(R.drawable.kakao_login_page)
-            subtitle3.setText("3. 실내식물 관리하기")
-            content3.setText(R.string.guide1_3)
-            image3.setImageResource(R.drawable.kakao_login_page)
-        }
-        else if(titlename == "과습된 식물 살리는 방법"){
-            bigtitle.setText(titlename)
-            subtitle1.setText("1. 과습 여부 파악하기")
-            content1.setText(R.string.guide2_1)
-            image1.setImageResource(R.drawable.kakao_login_page)
-            subtitle2.setText("2. 과습 식물 살리기")
-            content2.setText(R.string.guide2_2)
-            image2.setImageResource(R.drawable.kakao_login_page)
-            subtitle3.setText("3. 과습 후 관리하기")
-            content3.setText(R.string.guide2_3)
-            image3.setImageResource(R.drawable.kakao_login_page)
-        }
-        else if(titlename =="실내에서 선인장 기르는 방법"){
-            bigtitle.setText(titlename)
-            subtitle1.setText("1. 새로운 식물을 번식하기")
-            content1.setText(R.string.guide3_1)
-            image1.setImageResource(R.drawable.kakao_login_page)
-            subtitle2.setText("2. 선인장 보살피기")
-            content2.setText(R.string.guide3_2)
-            image2.setImageResource(R.drawable.kakao_login_page)
-            subtitle3.setText("3. 흔하게 발생하는 문제들을 해결하기")
-            content3.setText(R.string.guide3_3)
-            image3.setImageResource(R.drawable.kakao_login_page)
-        }
-        else if(titlename =="식물 건강하게 키우는 방법"){
-            bigtitle.setText(titlename)
-            subtitle1.setText("1. 올바른 환경 조성하기")
-            content1.setText(R.string.guide4_1)
-            image1.setImageResource(R.drawable.kakao_login_page)
-            subtitle2.setText("2. 물과 양분 제공")
-            content2.setText(R.string.guide4_2)
-            image2.setImageResource(R.drawable.kakao_login_page)
-            subtitle3.setText("3. 식물 키우는 간단한 요령")
-            content3.setText(R.string.guide4_3)
-            image3.setImageResource(R.drawable.kakao_login_page)
-        }
-        else if(titlename =="실내식물 관리하는 방법"){
-            bigtitle.setText(titlename)
-            subtitle1.setText("1. 식물에 물을 지속해서 주기")
-            content1.setText(R.string.guide5_1)
-            image1.setImageResource(R.drawable.kakao_login_page)
-            subtitle2.setText("2. 실내식물 관리하기")
-            content2.setText(R.string.guide5_2)
-            image2.setImageResource(R.drawable.kakao_login_page)
-            subtitle3.setText("3. 식물을 제대로 알기")
-            content3.setText(R.string.guide5_3)
-            image3.setImageResource(R.drawable.kakao_login_page)
-        }
-        else if(titlename =="다육이 잎꽂이 하는 방법"){
-            bigtitle.setText(titlename)
-            subtitle1.setText("1. 잎 떼어내 말리기")
-            content1.setText(R.string.guide6_1)
-            image1.setImageResource(R.drawable.kakao_login_page)
-            subtitle2.setText("2. 새로운 뿌리 돋아나게 하기")
-            content2.setText(R.string.guide6_2)
-            image2.setImageResource(R.drawable.kakao_login_page)
-            subtitle3.setText("3. 과습 후 관리하기")
-            content3.setText(R.string.guide6_3)
-            image3.setImageResource(R.drawable.kakao_login_page)
-        }
-        else if(titlename =="분갈이 하는 방법"){
-            bigtitle.setText(titlename)
-            subtitle1.setText("1. 과습 여부 파악하기")
-            content1.setText(R.string.guide7_1)
-            image1.setImageResource(R.drawable.kakao_login_page)
-            subtitle2.setText("2. 과습 식물 살리기")
-            content2.setText(R.string.guide7_2)
-            image2.setImageResource(R.drawable.kakao_login_page)
-            subtitle3.setText("3. 아기 다육이 옮겨 심고 키우기")
-            content3.setText(R.string.guide7_3)
-            image3.setImageResource(R.drawable.kakao_login_page)
-        }
-
-        back_button.setOnClickListener{
-            finish()
-        }
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.guideline_card_cell,parent,false)
+        return ViewHolder(view)
     }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val tv_title: TextView = itemView.findViewById(R.id.tv_title)
+        private val tv_subtitle: TextView = itemView.findViewById(R.id.tv_subtitle)
+
+        fun bind(item: GuidelineData) {
+            tv_title.text = item.title
+            tv_subtitle.text = item.subtitle
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, GuideLineDetailActivity::class.java)
+                intent.putExtra("title", item.title)
+                ContextCompat.startActivity(itemView.context, intent, null)
+            }
+        }
+    }
+
+
 }
+data class GuidelineData(
+    val title : String,
+    val subtitle : String
+)
+
